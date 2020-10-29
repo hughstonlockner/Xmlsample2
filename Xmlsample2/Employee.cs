a4729897fba32493217f4e40a846e799ad5dbf46
Hughston Lockner <hughston94@gmail.com>
2020-10-29 14:14:23 -0400

    Add project files.

---
+++ Xmlsample2/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Xmlsample2
+{
+    [Serializable]
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double ssNumber { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ Xmlsample2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Xmlsample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp = new Employee();
+
+            emp.ID = 123;
+            emp.Name = "Ninja Nerd";
+            emp.ssNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: {emp.Name} \nEmployee SS#: {emp.ssNumber} \nDate Created: {emp.EntryDate}");
+
+            string FilePath = "c:/Temp/";
+            string FileName = "Ninja.xml";
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            XmlSerializer ser = new XmlSerializer(typeof(Employee));
+
+            ser.Serialize(writer, emp);
+            writer.Close();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
